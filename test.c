9abaff18693af1dad997f424088fa3aab16c28f3
Krishna Prajapati <kjp189@scarletmail.rutgers.edu>
2020-12-07 19:28:59 -0500

    figuring out pipes

--- test.c
+++ test.c
@@ -1,7 +1,14 @@
+
+#include <stdbool.h>
 #include <stdlib.h>
-#include <stdio.h> 	/* for printf */
-#include <string.h>	/* for strlen */
-#include <errno.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <ctype.h>
 
 int
 main(int argc, char **argv)
@@ -28,11 +35,31 @@ main(int argc, char **argv)
 		perror("read");
 	exit(0);*/
 
+/*
 	char asdf[5] = "aadf";
 
 	printf("%s\n", asdf);
 	char* a = strtok(asdf, ">>");
 	char* b = strtok(NULL, ">");
 	printf("%s\n", a);
-	if(b==NULL)printf("ULLL");
+	if(b==NULL)printf("ULLL");*/
+	int n;
+	int fd[2];
+	fd[0] = 1;
+	fd[1] = open("test.txt", O_WRONLY | O_APPEND | O_CREAT, 00777);
+	char *data = "hello... this is sample data";
+	pipe(fd);
+	char buf[1025];
+	write(fd[1], data, strlen(data));
+	printf("This is a test");
+	if ((n = read(fd[0], buf, 1024)) >= 0){
+		buf[n] = 0;	// terminate the string
+		//buf2[m] = 0;
+        printf("read %d bytes from the pipe: \"%s\"\n", n, buf);
+       // printf("read %d bytes from the pipe: \"%s\"\n", m, buf2);
+	}	
+	else
+		perror("read");
+	close(fd[0]);
+	exit(0);
 }
